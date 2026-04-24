5fd51b792b49cd7e9c86d84a2d44f51681aa29b3
territory.io <forbusiness68.8.65.43@gmail.com>
2026-04-24 16:05:20 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set base to relative for deployment on main branch/subdirectory
+  base: './',
+  build: {
+    outDir: 'dist',
+  }
 })
